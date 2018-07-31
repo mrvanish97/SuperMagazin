e0ef9e38579267ceeef7ae0259fffef38d8693aa
uonagent <victormushtin@gmail.com>
2018-07-31 14:43:56 +0200

    ekk2

---
+++ app/src/main/java/com/uonagent/supermagazin/utils/ItemModel.kt
@@ -0,0 +1,41 @@
+package com.uonagent.supermagazin.utils
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class ItemModel(
+        var title: String = "",
+        var cost: Double = Double.NaN,
+        var description: String = "",
+        var photo: String = "",
+        var uid: String = ""
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readDouble(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeDouble(cost)
+        parcel.writeString(description)
+        parcel.writeString(photo)
+        parcel.writeString(uid)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ItemModel> {
+        override fun createFromParcel(parcel: Parcel): ItemModel {
+            return ItemModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ItemModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
